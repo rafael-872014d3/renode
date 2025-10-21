f06cd452a5fa721195bf3418ebb8711a1c31b4a6
Rafael Sekula <rafael.sekula@burkert.com>
2025-10-21 10:09:15 +0200

    Merge commit

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -u
 set -e
 
 ROOT_PATH="$(cd "$(dirname $0)"; echo $PWD)"
+WINDOWS_ROOT_PATH="$(cygpath -w "$ROOT_PATH")"
 export ROOT_PATH
 OUTPUT_DIRECTORY="$ROOT_PATH/output"
 EXPORT_DIRECTORY=""
@@ -58,6 +59,7 @@ function print_help() {
   echo "--tlib-export-compile-commands    build tlibs with 'compile_commands.json' (requires --external-lib-arch)"
   echo "--host-arch                       build with a specific tcg host architecture (default: i386)"
   echo "--skip-dotnet-target-generation   don't generate 'Directory.Build.targets' file, useful when experimenting with different build settings"
+  echo "--tcg-opcode-backtrace            collect a backtrace for each emitted TCG opcode, to track internal TCG errors (implies Debug configuration)"
   echo "<ARGS>                            arguments to pass to the build system"
 }
 
@@ -161,6 +163,12 @@ do
           ;;
         "skip-dotnet-target-generation")
           GENERATE_DOTNET_BUILD_TARGET=false
+          ;;
+        "tcg-opcode-backtrace")
+          # Doesn't make sense without debug.
+          CONFIGURATION="Debug"
+
+          CMAKE_COMMON+=" -DTCG_OPCODE_BACKTRACE=ON"
           ;;
         *)
           print_help
@@ -336,7 +344,7 @@ then
   find $ROOT_PATH -type f -name 'project.assets.json' -delete
 fi
 
-CORES_PATH="$ROOT_PATH/src/Infrastructure/src/Emulator/Cores"
+CORES_PATH="$WINDOWS_ROOT_PATH/src/Infrastructure/src/Emulator/Cores"
 
 # clean instead of building
 if $CLEAN
---
+++ env.bat
@@ -0,0 +1,7 @@
+@echo off
+set PATH=C:\Workspace\mingw64\bin;C:\cygwin64\bin;%PATH%
+C:
+chdir C:\Workspace\renode/renode
+
+bash --login -i
+ 
